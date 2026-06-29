603db91b920a39927d2cdce73e2abc9274cf3cb5
mohitsoni705 <mohitravet07@gmail.com>
2026-06-29 13:14:59 +0530

    fixed backend api

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://taskflow-yp9a.onrender.com/api/v1";
-export const BACKEND_URL = "http://localhost:8000/api/v1";
+export const BACKEND_URL = "https://taskflow-yp9a.onrender.com/api/v1";
+// export const BACKEND_URL = "http://localhost:8000/api/v1";
